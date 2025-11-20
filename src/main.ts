8b290e31b0e78ab68308c969efd836473fe8abfd
brattik <bulki86@gmail.com>
2025-11-20 08:59:53 +0300

    обновляет список моделей для использования в чате

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ let currentSessionId = localStorage.getItem('chatSessionId') ||
 localStorage.setItem('chatSessionId', currentSessionId);
 
 
-const modelList = ["CohereLabs/c4ai-command-r7b-arabic-02-2025:cohere"]
+const modelList = ["CohereLabs/c4ai-command-a-03-2025:cohere"]
 
 modelList.forEach(m => {
   const opt = document.createElement("option");
